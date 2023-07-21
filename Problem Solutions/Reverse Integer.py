23eaa138cb94631319190c343e629749221240f0
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2023-07-21 17:43:14 -0400

    Create Reverse Integer.py

---
+++ Problem Solutions/Reverse Integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverse(self, x):
+        x = str(x)
+        y = 1
+        if x[0] == '-':
+            y = -1
+            x = x[::-1]
+            x = int(x[0:len(x)-1]) * y
+            if int(x) > 2**31 - 1 or int(x) < -2**31:
+                return 0
+            return x
+        x = x[::-1]
+        if int(x) > 2**31 - 1 or int(x) < -2**31:
+                return 0
+        return int(x)
