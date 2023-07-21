444c8dacc987ab8ab2de08521327721eab8580ba
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2023-07-21 17:32:36 -0400

    Create Reverse Vowels of a String.py

---
+++ Problem Solutions/Reverse Vowels of a String.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        x = ''
+        for letter in s:
+            if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u' or letter == 'A' or letter == 'E' or letter == 'I' or letter == 'O' or letter == 'U':
+                x = x + letter 
+        vowels = x[::-1]
+        vowels_count = 0
+        res = ''
+        for letter in s:
+            if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u' or letter == 'A' or letter == 'E' or letter == 'I' or letter == 'O' or letter == 'U':
+                res = res + vowels[vowels_count]
+                vowels_count +=1
+            else:
+                res = res + letter
+        return res    
