5593302af4e8813dbb200b7fc72a7fe85937e670
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:55:43 -0400

    Create 2238-ANumberAfterADoubleReversal

---
+++ 2238-ANumberAfterADoubleReversal/2238-ANumberAfterADoubleReversal.py
@@ -0,0 +1,22 @@
+# Last updated: 9/10/2025, 12:55:42 AM
+class Solution(object):
+    def isSameAfterReversals(self, num):
+        y = str(num)
+        reversed1 = y[::-1]
+        reversed12 = int(reversed1)
+        reversed13 = str(reversed12)
+        reversed2 = reversed13[::-1]
+        reversed14 = int(reversed2)
+        reversed15 = str(reversed14)
+
+        if y == reversed15:
+            return True
+        else:
+            return False
+
+        
+        
+        """
+        :type num: int
+        :rtype: bool
+        """
