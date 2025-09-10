56a58c4884ee4d1d980f1c0efd6f9d94887df4fb
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:56:39 -0400

    Create 1806-CountOfMatchesInTournament

---
+++ 1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.py
@@ -0,0 +1,10 @@
+# Last updated: 9/10/2025, 12:56:38 AM
+class Solution(object):
+    def numberOfMatches(self, n):
+        return n-1
+        
+        
+        """
+        :type n: int
+        :rtype: int
+        """
