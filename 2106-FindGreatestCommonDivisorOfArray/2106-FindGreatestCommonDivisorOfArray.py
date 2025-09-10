1e428c9bbefd240360a0782765ae26087a07fe3b
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:55:53 -0400

    Create 2106-FindGreatestCommonDivisorOfArray

---
+++ 2106-FindGreatestCommonDivisorOfArray/2106-FindGreatestCommonDivisorOfArray.py
@@ -0,0 +1,11 @@
+# Last updated: 9/10/2025, 12:55:52 AM
+class Solution(object):
+    def findGCD(self, nums):
+        maxnum = max(nums)
+        minnum = min(nums)
+        ans = 0
+        for i in range(1, minnum+1):
+            if minnum % i == 0 and maxnum % i == 0 and i > ans:
+                ans = i
+        return ans
+        
