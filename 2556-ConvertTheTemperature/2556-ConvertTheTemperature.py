d9b6172c6aa1f503667af00e3fcfa2c57a26178a
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:55:21 -0400

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.py
@@ -0,0 +1,11 @@
+# Last updated: 9/10/2025, 12:55:20 AM
+class Solution(object):
+    def convertTemperature(self, celsius):
+       #ans = [celsius + 273.15, celsius * 1.80 + 32.00]
+        return [celsius + 273.15, celsius * 1.80 + 32.00]
+        
+        
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
