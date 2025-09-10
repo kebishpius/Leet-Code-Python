d2fd0ff93388b783fcddda2ccf1afa064813f3d2
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:57:11 -0400

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.py
@@ -0,0 +1,10 @@
+# Last updated: 9/10/2025, 12:57:10 AM
+class Solution(object):
+    def sortArray(self, nums):
+        x = sorted(nums)
+        return x
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
