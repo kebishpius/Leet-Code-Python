61014e7c34fcfd06632f4ac10c099a3a6fa1cc55
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:58:17 -0400

    Create 153-FindMinimumInRotatedSortedArray

---
+++ 153-FindMinimumInRotatedSortedArray/153-FindMinimumInRotatedSortedArray.py
@@ -0,0 +1,8 @@
+# Last updated: 9/10/2025, 12:58:17 AM
+class Solution(object):
+    def findMin(self, nums):
+        return min(nums)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
