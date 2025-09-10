eff98ba1b15b4f9d46b9095ab0c6df8e0551971d
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:55:25 -0400

    Create 2486-MostFrequentEvenElement

---
+++ 2486-MostFrequentEvenElement/2486-MostFrequentEvenElement.py
@@ -0,0 +1,28 @@
+# Last updated: 9/10/2025, 12:55:24 AM
+class Solution(object):
+    def mostFrequentEven(self, nums):
+        nodupes = list(dict.fromkeys(nums))
+        onlyevens = []
+        for i in range(len(nodupes)):
+            if nodupes[i] % 2 == 0:
+                onlyevens.append(nodupes[i])
+        if len(onlyevens) == 0:
+            return -1
+        if len(nums) == 1:
+            return nums[0]
+        onlyevens.sort()
+        max = -99
+        tracker = 0
+        print(onlyevens)
+        for j in(range(0, len(onlyevens))):
+            if nums.count(onlyevens[j]) > max:
+                max = nums.count(onlyevens[j])
+                tracker = onlyevens[j]
+        return tracker
+
+
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
