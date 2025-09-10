52516c38e5734f8bb7458b1b6a372bdebcbc2dca
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:55:06 -0400

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.py
@@ -0,0 +1,10 @@
+# Last updated: 9/10/2025, 12:55:05 AM
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        return num + 2*t
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        
