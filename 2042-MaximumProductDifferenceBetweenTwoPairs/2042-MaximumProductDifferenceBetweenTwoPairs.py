9b774a587ef8806619622c2a95ae2c90096aa3b2
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:55:59 -0400

    Create 2042-MaximumProductDifferenceBetweenTwoPairs

---
+++ 2042-MaximumProductDifferenceBetweenTwoPairs/2042-MaximumProductDifferenceBetweenTwoPairs.py
@@ -0,0 +1,9 @@
+# Last updated: 9/10/2025, 12:55:58 AM
+class Solution(object):
+    def maxProductDifference(self, nums):
+        nums.sort()
+        return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
