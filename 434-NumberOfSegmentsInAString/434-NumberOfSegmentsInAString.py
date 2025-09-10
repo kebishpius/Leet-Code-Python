be5edce2e1f054d78e7fb1de1d60a2ccba6f90c7
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-09-10 00:57:54 -0400

    Create 434-NumberOfSegmentsInAString

---
+++ 434-NumberOfSegmentsInAString/434-NumberOfSegmentsInAString.py
@@ -0,0 +1,10 @@
+# Last updated: 9/10/2025, 12:57:54 AM
+class Solution(object):
+    def countSegments(self, s):
+        x = s.split()
+        return len(x)
+        
+        """
+        :type s: str
+        :rtype: int
+        """
