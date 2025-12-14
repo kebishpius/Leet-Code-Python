d4ec56a957a2e74bc280dcc58e5533e54cfc40f7
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-12-14 15:11:34 -0500

    File updated at 12/14/2025, 3:11:34 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,22 +1,22 @@
-# Last updated: 12/14/2025, 3:10:16 PM
+# Last updated: 12/14/2025, 3:11:34 PM
 1class Solution(object):
 2    def findMaxConsecutiveOnes(self, nums):
-3        max = 0
-4        one_counter = 0
-5        for i in range(len(nums)):
-6            if nums[i] == 1:
-7                one_counter += 1
-8            if nums[i] != 1 or i == len(nums) - 1:
-9                if one_counter > max:
-10                    max = one_counter
-11                one_counter = 0
-12            print(one_counter)
-13            print(max)
-14            print()
-15        return max
-16                    
-17            
-18        """
-19        :type nums: List[int]
-20        :rtype: int
-21        """
+3        maxCount = 0
+4        count = 0
+5        for i in nums:
+6            if i != 0:
+7                count += 1
+8            else:
+9                if maxCount < count:
+10                    maxCount = count
+11                count = 0
+12        if maxCount < count:
+13            maxCount = count
+14        return maxCount
+15                
+16
+17        """
+18        :type nums: List[int]
+19        :rtype: int
+20        """
+21        
