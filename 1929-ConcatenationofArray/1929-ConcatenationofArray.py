a1c9fb611f1df03fd2f4a7101060a93e07f6f147
Kebish Pius <63826489+kebishpius@users.noreply.github.com>
2025-12-14 15:10:16 -0500

    File updated at 12/14/2025, 3:10:16 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,14 +1,22 @@
-# Last updated: 12/14/2025, 10:30:54 AM
+# Last updated: 12/14/2025, 3:10:16 PM
 1class Solution(object):
-2    def shuffle(self, nums, n):
-3        ans = []
-4        for i in range(n):
-5            ans.append(nums[i])
-6            ans.append(nums[i+n])
-7        return ans
-8        """
-9        :type nums: List[int]
-10        :type n: int
-11        :rtype: List[int]
-12        """
-13        
+2    def findMaxConsecutiveOnes(self, nums):
+3        max = 0
+4        one_counter = 0
+5        for i in range(len(nums)):
+6            if nums[i] == 1:
+7                one_counter += 1
+8            if nums[i] != 1 or i == len(nums) - 1:
+9                if one_counter > max:
+10                    max = one_counter
+11                one_counter = 0
+12            print(one_counter)
+13            print(max)
+14            print()
+15        return max
+16                    
+17            
+18        """
+19        :type nums: List[int]
+20        :rtype: int
+21        """
